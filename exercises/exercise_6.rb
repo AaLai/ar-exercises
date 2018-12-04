66111706451b30e3fd622488d9f834e8a2738591
aaron <aaron_lai@hotmail.com>
2018-12-04 23:34:37 +0000

    completed 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -4,8 +4,16 @@ require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
+require 'faker'
 
 puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+10.times do
+  @store1.employees.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, hourly_rate: rand(15..100))
+end
+
+10.times do
+  @store2.employees.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, hourly_rate: rand(15..100))
+end
